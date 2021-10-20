411ada7f8720658c202b6d6efbac7bca53d56390
JesusSePe <umbreon20019@gmail.com>
2021-10-20 20:13:00 +0200

    first commit

---
+++ app/src/main/java/com/example/camara/MainActivity.java
@@ -0,0 +1,72 @@
+package com.example.camara;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.util.Optional;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button trigger = findViewById(R.id.trigger);
+        trigger.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dispatchTakePictureIntent();
+            }
+        });
+    }
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    public void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        try {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        } catch (ActivityNotFoundException e) {
+            // display error state to the user
+
+            Toast.makeText(this, "No es pot obrir la càmera", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        final ImageView imageView = findViewById(R.id.imageView);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imageView.setImageBitmap(imageBitmap);
+
+            // Guardar a un fitxer
+            File path = getApplicationContext().getFilesDir();
+            System.out.println(path);
+            File foto = new File(path, "foto.jpg");
+
+
+            try {
+                FileOutputStream os = new FileOutputStream(foto);
+                imageBitmap.compress(Bitmap.CompressFormat.JPEG, 100, os);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+                Toast.makeText(this, "Error al guardar la foto", Toast.LENGTH_SHORT).show();
+            }
+
+        }
+    }
+}
