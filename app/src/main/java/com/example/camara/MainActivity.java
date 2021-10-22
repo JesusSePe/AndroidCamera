5d7e6bcfe912f32d85250349816ed10de1a297c9
JesusSePe <umbreon20019@gmail.com>
2021-10-22 17:34:42 +0200

    HD update

--- app/src/main/java/com/example/camara/MainActivity.java
+++ app/src/main/java/com/example/camara/MainActivity.java
@@ -1,12 +1,17 @@
 package com.example.camara;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.media.Image;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -15,6 +20,7 @@ import android.widget.Toast;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.Optional;
 
 public class MainActivity extends AppCompatActivity {
@@ -35,38 +41,46 @@ public class MainActivity extends AppCompatActivity {
     static final int REQUEST_IMAGE_CAPTURE = 1;
     public void dispatchTakePictureIntent() {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        try {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-        } catch (ActivityNotFoundException e) {
-            // display error state to the user
-
-            Toast.makeText(this, "No es pot obrir la càmera", Toast.LENGTH_SHORT).show();
+        // Ensure that there's a camera activity to handle the intent
+        Log.i("FUNCIÓ", "Entrant a dispatchTakePictureIntent");
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            Log.i("FUNCIÓ", "Entrant a dispatchTakePictureIntent2");
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = getFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.example.android.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+                Toast.makeText(this, "Start Activity Camera", Toast.LENGTH_SHORT);
+            }
         }
+    }
+
+    protected File getFile() throws IOException{
+        // Guardar a un fitxer
+        File path = getApplicationContext().getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File foto = new File(path, "imatge.jpg");
+        return foto;
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         final ImageView imageView = findViewById(R.id.imageView);
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
-            imageView.setImageBitmap(imageBitmap);
-
-            // Guardar a un fitxer
-            File path = getApplicationContext().getFilesDir();
-            System.out.println(path);
-            File foto = new File(path, "foto.jpg");
-
-
-            try {
-                FileOutputStream os = new FileOutputStream(foto);
-                imageBitmap.compress(Bitmap.CompressFormat.JPEG, 100, os);
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-                Toast.makeText(this, "Error al guardar la foto", Toast.LENGTH_SHORT).show();
-            }
-
+        try {
+            Uri fileUri = Uri.fromFile(getFile());
+            imageView.setImageURI(fileUri);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+
     }
 }
